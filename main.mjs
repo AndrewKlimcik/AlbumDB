1b1d9cd69910934a46e88569a565d4eede1369bf
Andrej Klimcik <Andrej.Klimcik@rsm.cz>
2024-05-14 23:08:09 +0200

    Added EJS files and other configurations. TODO detail - finish loading album data, finish loading specific album

--- main.mjs
+++ main.mjs
@@ -1,18 +1,57 @@
 import express from 'express'
-
+import knex from 'knex'
+import knexfile from './knexfile.js'
 
 const app = express()
+const db = knex(knexfile)
 
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 
+// page definitions
 app.get('/', async (req, res) => {
     res.render('index', {
         title: 'AlbumDB',
         description: 'Music database'
     })
+})
+app.get('/search/', async (req, res) => {
+    res.render('search', {
+        // title: 'AlbumDB',
+        // description: 'Music database'
+    })
+})
+app.get('/detail/:id', async (req, res, next) => {
+    const album = await db('albums').select('*').where('id', req.params.id).first()
+
+    if (!album) return next()
+
+    res.render('detail', {
+        album,
+    })
+})
+app.get('/add/', async (req, res) => {
+    res.render('add', {
+        // title: 'AlbumDB',
+        // description: 'Music database'
+    })
+})
+
+
+app.post('/add-album', async (req, res) => {
+    const album = {
+        title: req.body.title,
+        artists: req.body.artists,
+        songs: req.body.artists,
+        year: req.body.year,
+        genre: req.body.genre
+    }
+
+    await db('albums').insert(album);
+
+    res.redirect('/');
 })
 
 
